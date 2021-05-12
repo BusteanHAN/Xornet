9d19b8ca6d10f84873eee87c6644e54de97b53a5
Connor <cimok2000@gmail.com>
2021-05-12 14:17:03 +0100

    added comments and made it more efficient

--- reporter/trythis.js
+++ reporter/trythis.js
@@ -4,45 +4,51 @@ const os = require("os");
 let static = {};
 const version = 0.09;
 
+// This will get information about all the drives on the system
 async function getDriveInformation() {
-  drives = [];
-  static = await (await si.getStaticData()).diskLayout;
-  for (x in static) {
-    drives.push([
-      static[x].type,
-      static[x].name,
-      static[x].size / 1024 / 1024 / 1024
-    ]);
-    total = total + static[x].size;
-    x += 1;
-  }
-  return drives;
+    drives = []; // Create an array for the drives to be stored within
+    static = await (await si.diskLayout());
+    // This will loop through all the drives and push the data into the drives Array
+    for (x in static) {
+        drives.push([
+            static[x].type,
+            static[x].name,
+            static[x].size
+        ]);
+        x += 1; // Increments the loop
+    }
+    return drives;
 }
 
 async function getStats() {
-  const name = os.hostname();
-  const platform = os.platform();
+    const name = os.hostname();
+    const platform = os.platform();
 
-  valueObject = {
-    networkStats: `(*) tx_sec, rx_sec`,
-    currentLoad: "currentLoad"
-  };
+    valueObject = {
+        networkStats: `(*) tx_sec, rx_sec`,
+        currentLoad: "currentLoad"
+    };
 
-  const data = await si.get(valueObject);
+    const data = await si.get(valueObject);
 
-  let stats = {
-    static,
-    name,
-    platform,
-    ram: {
-      total: os.totalmem(),
-      free: os.freemem()
-    },
-    cpu: data.currentLoad.currentLoad,
-    network: data.networkStats,
-    reporterVersion: version,
-    drives: await getDriveInformation()
-  };
+    let stats = {
+        static,
+        name,
+        platform,
+        ram: {
+            total: os.totalmem(),
+            free: os.freemem()
+        },
+        cpu: data.currentLoad.currentLoad,
+        network: data.networkStats,
+        reporterVersion: version,
+        drives: await getDriveInformation()
+    };
 
-  return stats;
+    return stats;
 }
+
+// For Testing
+// getStats().then(data => {
+//     console.log(data);
+// })
