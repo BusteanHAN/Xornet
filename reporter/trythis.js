0c48066f9093d7e6715e99639e91cc9d4217656c
Connor <cimok2000@gmail.com>
2021-05-12 10:20:08 +0100

    created trythis.js which is a new version of the reporter
    This version of the reporter gets the drive information of all drives, including what type of drive (SSD etc), the name of the drive and its size in GBs.

---
+++ reporter/trythis.js
@@ -0,0 +1,50 @@
+const si = require("systeminformation");
+const os = require("os");
+
+let static = {};
+const version = 0.09;
+
+async function getDriveInformation() {
+  x = 0;
+  total = 0;
+  drives = [];
+  static = await (await si.getStaticData()).diskLayout;
+  for (x in static) {
+    drives.push([
+      static[x].type,
+      static[x].name,
+      static[x].size / 1024 / 1024 / 1024
+    ]);
+    total = total + static[x].size;
+    x += 1;
+  }
+  return drives;
+}
+
+async function getStats() {
+  const name = os.hostname();
+  const platform = os.platform();
+
+  valueObject = {
+    networkStats: `(*) tx_sec, rx_sec`,
+    currentLoad: "currentLoad"
+  };
+
+  const data = await si.get(valueObject);
+
+  let stats = {
+    static,
+    name,
+    platform,
+    ram: {
+      total: os.totalmem(),
+      free: os.freemem()
+    },
+    cpu: data.currentLoad.currentLoad,
+    network: data.networkStats,
+    reporterVersion: version,
+    drives: await getDriveInformation()
+  };
+
+  return stats;
+}
