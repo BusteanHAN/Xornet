e20208fbfcda98b7c798ad963617fa6057bec9ad
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-12 14:43:59 +0300

    fixed issues with the linux install script

--- reporter/install-linux/install.sh
+++ reporter/install-linux/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # Disable service
+sudo rm ./install.sh
+
+# Disable service
 systemctl stop xornet
 echo "Xornet service stopped!"
 systemctl disable xornet
@@ -9,7 +12,7 @@ echo "Xornet service disabled!"
 # Delete all files
 sudo rm -rf /etc/xornet
 sudo rmdir /etc/xornet
-sudo rm /etc/systemd$systemctl/xornet.service
+sudo rm /etc/systemd/system/xornet.service
 echo "Xornet uninstalled!"
 
 # Create folder again
@@ -21,7 +24,8 @@ chmod +x /etc/xornet/xornet-reporter-v0.10.bin
 echo "Xornet reporter downloaded!"
 
 # Download service
-sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/841991161513639956/xornet.service" -P /etc/systemd$systemctl enable xornet
+sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/842001342381555712/xornet.service" -P /etc/systemd
+systemctl enable xornet
 echo "Xornet service downloaded!"
 
 # Register service
