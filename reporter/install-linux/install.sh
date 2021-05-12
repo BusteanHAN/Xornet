a474dfbb7424a7f9174a1e28b2843ae72c211d71
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-12 13:56:20 +0300

    improved linux install script & updated to v0.10

--- reporter/install-linux/install.sh
+++ reporter/install-linux/install.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
+# Disable service
+systemctl stop xornet
+echo "Xornet service stopped!"
+systemctl disable xornet
+echo "Xornet service disabled!"
+
+# Delete all files
+sudo rm -rf /etc/xornet
+sudo rmdir /etc/xornet
+sudo rm /etc/systemd$systemctl/xornet.service
+echo "Xornet uninstalled!"
+
+# Create folder again
 mkdir /etc/xornet
-wget "https://github.com/Geoxor/Xornet/releases/download/v0.09/xornet-reporter-v0.09.bin.09" -P /etc/xornet
-chmod +x /etc/xornet/xornet-reporter-v0.09.bin.09
+
+# Download
+wget "https://github.com/Geoxor/Xornet/releases/download/v0.10/xornet-reporter-v0.10.bin" -P /etc/xornet
+chmod +x /etc/xornet/xornet-reporter-v0.10.bin
 echo "Xornet reporter downloaded!"
-sudo wget "https://cdn.discordapp.com/attachments/808856125817618532/841671047011106836/xornet.service" -P /etc/systemd$systemctl enable xornet
+
+# Download service
+sudo wget "https://cdn.discordapp.com/attachments/806300597338767450/841991161513639956/xornet.service" -P /etc/systemd$systemctl enable xornet
+echo "Xornet service downloaded!"
+
+# Register service
 systemctl start xornet
 systemctl status xornet
+echo "Xornet install finished!"
