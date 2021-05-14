f2646239d352423cda35511aefe8027de72e807d
N1kO23 <niko.huuskonen.00@gmail.com>
2021-05-14 22:31:42 +0300

    Made the initial database models

--- server/models/Machine.js
+++ server/models/Machine.js
@@ -5,7 +5,8 @@ mongoose.connect('mongodb://localhost:27017/xornet', {useNewUrlParser: true, use
 const schema = new Schema({
     _id:  String, // String is shorthand for {type: String}
     machine_id:     String,
-    details:        { type: Object, default: null},
+    static:         { type: Object, default: null},
+    
 }, {
     versionKey: false // You should be aware of the outcome after set to false
 });
---
+++ server/models/Stats.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost:27017/xornet', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const schema = new Schema({
+    _id:            String,                         // The mongo object ID
+    machine_id:     String,                         // The machine's ID
+    ram:            { type: Object, default: null}, // The machines current ram usage
+    cpu:            { type: Object, default: null}, // The machines current cpu usage
+    network:        { type: Object, default: null}, // The machines current network usage
+    disks:          { type: Object, default: null}, // The machines current disk capacity
+    
+}, {
+    versionKey: false // You should be aware of the outcome after set to false
+});
+
+module.exports = mongoose.model('Stats', schema);
